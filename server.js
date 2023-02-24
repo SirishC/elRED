cc16f45cd6f0dd089b7d4270a3fa4294654330c6
Sirish C <chejerlasirish@gmail.com>
2023-02-24 15:42:44 +0530

    added API documentation

--- server.js
+++ server.js
@@ -24,6 +24,12 @@ const taskRouter = require('./routers/tasks.router.js');
  
 app.use('/api/users',userRouter);
 app.use('/api/task',taskRouter);
+
+// API Documentation 
+app.get("/", (req, res) => {
+    res.status(301).redirect("https://documenter.getpostman.com/view/4446750/2s93CNNtYA#628a974c-6952-4ff3-817c-56f1b6d5a57e")
+})
+
 const PORT = process.env.PORT || 3322;
 app.listen(PORT,()=>{
     console.log(`Application running on PORT: ${PORT}`);
